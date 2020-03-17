b4b7886f005be0252bdc9565933afa6ccf5df32d
jccm17 <40042640+jccm17@users.noreply.github.com>
2020-03-17 18:48:21 -0400

    upload app

---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+
+void main() => runApp(MaterialApp(home: Main()));
+class Main extends StatefulWidget {
+  @override
+  _CovitVirusState createState() => _CovitVirusState();
+}
+
+class _CovitVirusState extends State<Main> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  //final Set<String> _favorites = Set<String>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: WebView(
+        initialUrl: 'https://www.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+        },
+      ),
+     // floatingActionButton: _bookmarkButton(),
+    );
+  }
+
+ /* _bookmarkButton() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async {
+              var url = await controller.data.currentUrl();
+              _favorites.add(url);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text('Saved $url for later reading.')),
+              );
+            },
+            child: Icon(Icons.favorite),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}*/
+
+/*class Menu extends StatelessWidget {
+  Menu(this._webViewControllerFuture, this.favoritesAccessor);
+  final Future<WebViewController> _webViewControllerFuture;
+  // TODO(efortuna): Come up with a more elegant solution for an accessor to this than a callback.
+  final Function favoritesAccessor;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (!controller.hasData) return Container();
+        return PopupMenuButton<String>(
+          onSelected: (String value) async {
+            if (value == 'Email link') {
+              var url = await controller.data.currentUrl();
+              await launch(
+                  'mailto:?subject=Check out this cool Wikipedia page&body=$url');
+            } else {
+              var newUrl = await Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return FavoritesPage(favoritesAccessor());
+              }));
+              Scaffold.of(context).removeCurrentSnackBar();
+              if (newUrl != null) controller.data.loadUrl(newUrl);
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+                const PopupMenuItem<String>(
+                  value: 'Email link',
+                  child: Text('Email link'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'See Favorites',
+                  child: Text('See Favorites'),
+                ),
+              ],
+        );
+      },
+    );
+  }
+}*/
+
+/*class FavoritesPage extends StatelessWidget {
+  FavoritesPage(this.favorites);
+  final Set<String> favorites;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Paginas Favoritas')),
+      body: ListView(
+          children: favorites
+              .map((url) => ListTile(
+                  title: Text(url), onTap: () => Navigator.pop(context, url)))
+              .toList()),
+    );
+  }
+}*/
+
+/*class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: true),
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: false),
+            ),
+          ],
+        );
+      },
+    );
+  }
+*/
+  navigate(BuildContext context, WebViewController controller,
+      {bool goBack: false}) async {
+    bool canNavigate =
+        goBack ? await controller.canGoBack() : await controller.canGoForward();
+    if (canNavigate) {
+      goBack ? controller.goBack() : controller.goForward();
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+            content: Text("No ${goBack ? 'back' : 'forward'} Historial item")),
+      );
+    }
+  }
+}
